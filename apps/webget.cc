5e6d4c9566a0f39ff9c6d3d9f7da1b0ef7c0b89b
zhaozhiyangzzy <zzybry@163.com>
2025-06-22 09:38:10 +0000

    testwq

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,6 +9,7 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
+  
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
   cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
